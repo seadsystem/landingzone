0fa35ef31fe9be845cc0b13259d52646270c991c
Ian Lofgren <ianl.lofgren@tuul.com>
2015-10-14 00:32:09 -0700

    initial commit, beginning to migrate deployment scipts from backend to landingzone

---
+++ deploy/init.pp
@@ -0,0 +1,49 @@
+class deploy {
+  import 'inituser'
+
+  # Postgres table data_raw
+  $table_schema =  '
+CREATE TABLE data_raw (
+  serial BIGINT NOT NULL,
+  type CHAR(1) NOT NULL,
+  device TEXT NULL,
+  data BIGINT NOT NULL,
+  time TIMESTAMP NOT NULL
+);
+CREATE TABLE classifications (
+  Serial BIGINT NOT NULL,
+  StartTime TIMESTAMP NOT NULL,
+  EndTime TIMESTAMP NOT NULL,
+  Classification TEXT NOT NULL
+);
+  '
+
+  exec {'seads-db':
+    command => "sudo -u postgres psql -c 'CREATE DATABASE seads;'",
+    unless  => 'sudo -u postgres psql -l seads',
+    require => Class['postgres'],
+  }
+
+  db_user {'landingzone':
+    password => '',
+    require  => Exec['seads-db'],
+  }
+
+  exec {'create-table':
+    command => "sudo -u postgres psql -d seads -c '${table_schema}'",
+    unless  => "sudo -u postgres psql -d seads -c '\\d data_raw;'",
+    require => Exec['seads-db'],
+  }
+
+  exec {'enable-tablefunc':
+    command => 'sudo -u postgres psql -d seads -c "CREATE EXTENSION IF NOT EXISTS tablefunc;"',
+    require => Exec['seads-db'],
+  }
+
+  exec {'deploy.sh':
+    command   => '/etc/puppet/modules/deploy/files/deploy.sh',
+    #cwd       => '/vagrant',
+    require   => Exec['create-table'],
+  }
+}
+
---
+++ deploy/initpostgres.pp
@@ -0,0 +1,60 @@
+nstall postgresql 9.4
+class postgres {
+  $ppa_source = 'anonscm.debian.org/loggerhead/pkg-postgresql/postgresql-common/trunk/download/head:/apt.postgresql.org.s-20130224224205-px3qyst90b3xp8zj-1/apt.postgresql.org.sh'
+
+  # Install postgresql ubuntu ppa
+  exec {'installppa':
+    command => "curl -ssL ${ppa_source} | bash -x",
+    unless  => 'ls /etc/apt/sources.list.d/pgdg.list',
+  }
+
+  # Enable postgres updates to 9.4
+  exec {
+    'use-9.4':
+      command => "sed -i '/^[^#]/ s/$/ 9.4/' /etc/apt/sources.list.d/pgdg.list",
+      require => Exec['installppa'],
+      unless  => "grep '9.4' /etc/apt/sources.list.d/pgdg.list";
+    'update-apt':
+      command => 'apt-get update',
+      require => Exec['use-9.4']
+  }
+
+  # Install postgres 9.4 and extensions
+  package {'postgresql-9.4':
+    ensure  => installed,
+    require => Exec['update-apt'],
+  }
+  package {'postgresql-contrib-9.4':
+    ensure  => installed,
+    require => Exec['update-apt'],
+  }
+  package {'postgresql-plpython-9.4':
+    ensure  => installed,
+    require => Exec['update-apt'],
+  }
+
+  # Set up postgres configuration
+  file {'postgresql.conf':
+    ensure  => file,
+    owner   => 'postgres',
+    group   => 'postgres',
+    mode    => '0644',
+    path    => '/etc/postgresql/9.4/main/postgresql.conf',
+    source  => 'puppet:///modules/postgres/postgresql.conf',
+    require => Package['postgresql-9.4'],
+  }
+
+  # Ensure service starts at boot
+  service {'postgresql':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['postgresql.conf'],
+  }
+
+  file {'/var/run/postgresql':
+    ensure  => present,
+    owner   => 'postgres',
+    group   => 'db',
+    require => Package['postgresql-9.4'],
+  }
+}
